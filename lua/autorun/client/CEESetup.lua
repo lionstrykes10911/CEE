8f777c79603e9c79a3e3de834521cf3401ae790f
Mr. Nuke <140557157+lionstrykes10911@users.noreply.github.com>
2024-12-03 14:05:15 -0300

    Basic ui stuff

--- lua/autorun/client/CEESetup.lua
+++ lua/autorun/client/CEESetup.lua
@@ -1,8 +1,45 @@
-Catalyst = Catalyst
+Catalyst = Catalyst or {}
 
-print("Catalyst Loaded")
+local function openEditor()
+	local CEEScreenSpace = vgui.Create( "DFrame" )
+	CEEScreenSpace:SetTitle(" ")
+	CEEScreenSpace:MakePopup()
+	CEEScreenSpace:KillFocus()
+	CEEScreenSpace:SetSize( surface.ScreenWidth(),surface.ScreenHeight() )
+	CEEScreenSpace:SetScreenLock( true )
+	CEEScreenSpace.Paint = function( self, w, h )
+		draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 0) )
+	end
+	local EditorFrame = vgui.Create( "DFrame" )
+	EditorFrame:SetTitle( "Catalyst Entity Editor" )
+	EditorFrame:SetParent( CEEScreenSpace )
+	EditorFrame:SetSize( 250,surface.ScreenHeight() )
+	EditorFrame:SetSizable( true )
+	EditorFrame:SetMinimumSize( 200, surface.ScreenHeight() )
+	EditorFrame:SetMinWidth(200)
+	EditorFrame:SetScreenLock( true )
+	EditorFrame:MakePopup()
+	EditorFrame:ShowCloseButton( false )
+	EditorFrame.Paint = function( self, w, h )
+		draw.RoundedBox( 0, 0, 0, w, h, Color( 131, 131, 131, 225) )
+	end
+	EditorFrame:SetPaintBorderEnabled( true )
+end
+
+local function CatalystCheck() 
+	openEditor()
+end
+
+concommand.Add("cee_close", function(args)
+	CloseDermaMenus()
+end)
+
+concommand.Add("cee_open", function(args)
+	CatalystCheck()
+end)
 
 local icon = "icon64/CEEIcon_temp.png"
+icon = file.Exists("materials/"..icon,'GAME') and icon or "icon64/playermodel.png"
 
 list.Set( "DesktopWindows", "CEEditor",
 		{

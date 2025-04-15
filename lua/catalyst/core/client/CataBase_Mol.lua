4004233f7b6f968ce9b4d4752366d35801ff8248
Mr. Nuke <140557157+lionstrykes10911@users.noreply.github.com>
2025-04-15 10:11:44 -0300

    Update CataBase_Mol.lua
    Theorising code...

--- lua/catalyst/core/client/CataBase_Mol.lua
+++ lua/catalyst/core/client/CataBase_Mol.lua
@@ -1,1 +1,17 @@
 local Catalyst = Catalyst or {}
+local hidden = false
+local MOL = MOL
+
+
+--theorised hide code, should make it so we only throw a check when things actually change
+--instead of doing iterated table checks
+--[[while self:isValid() do
+  if self.hidden ~= MOL:isHidden(self) then
+      self.hidden = false
+  else
+      self.hidden = true
+  end
+else
+end
+end
+]]--

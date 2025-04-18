a2de76d3e5b42176cb98599323ef4434fd0350e3
Mr. Nuke <140557157+lionstrykes10911@users.noreply.github.com>
2025-04-18 12:42:34 -0300

    Update CataBase_Mol.lua

--- lua/catalyst/core/client/CataBase_Mol.lua
+++ lua/catalyst/core/client/CataBase_Mol.lua
@@ -1,6 +1,7 @@
 local Catalyst = Catalyst or {}
 local hidden = false
 local MOL = MOL
+local parts = parts or {}
 
 
 --theorised hide code (will be revised later, don't have a heart attack), should make it so we only throw a check when things actually change
@@ -14,8 +15,10 @@ end
 while self:isValid() do
   if self.hidden ~= Catalyst:isHidden(self or parent) then
       self.hidden = false
+      table.insert(parts, self)
   else
       self.hidden = true
+      table.insert(parts, PInd)
   end
 else
 end
